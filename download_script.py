a514ebd7ef3eb159a0c32c3b1864bb797165961b
Alessotech <112171477+Alessotech@users.noreply.github.com>
2025-01-23 21:48:18 +0300

    Update download_script.py

--- download_script.py
+++ download_script.py
@@ -1,4 +1,4 @@
- from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from selenium import webdriver
 from selenium.webdriver.common.by import By
