787ca503e623917f29dc8e5592a36e490b3eb84b
Alessotech <112171477+Alessotech@users.noreply.github.com>
2025-01-24 20:56:02 +0300

    Update download_script.py

--- download_script.py
+++ download_script.py
@@ -10,16 +10,23 @@ app = FastAPI()
 def setup_webdriver():
     """Set up the Chrome WebDriver with necessary options."""
     options = Options()
-    options.add_argument('--no-sandbox')
     options.add_argument('--headless')  # Run in headless mode
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')  # Required for Docker containers
+    options.add_argument('--disable-dev-shm-usage')  # Prevent shared memory issues
+    options.add_argument('--disable-gpu')  # Disable GPU rendering
     options.add_argument('--disable-extensions')
-    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')  # Debugging port for headless mode
+    options.add_argument('--disable-setuid-sandbox')
+    options.add_argument('--disable-software-rasterizer')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-background-networking')
+    options.add_argument('--disable-background-timer-throttling')
+    options.add_argument('--disable-renderer-backgrounding')
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument('--user-agent={}'.format(random.choice(["User-Agent1", "User-Agent2"])))
 
     # Use the Service class to specify the ChromeDriver path
-    chromedriver_path = "/opt/render/project/.render/chromedriver"  # Path to ChromeDriver
+    chromedriver_path = "/opt/render/project/.render/chromedriver"
     service = Service(chromedriver_path)
 
     return webdriver.Chrome(service=service, options=options)
