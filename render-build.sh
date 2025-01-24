0691f61383389fda5dd72e7cd8b8d39a9242e091
Alessotech <112171477+Alessotech@users.noreply.github.com>
2025-01-24 20:15:30 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 STORAGE_DIR=/opt/render/project/.render
@@ -10,9 +11,22 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
   rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src
+  cd $HOME/project/src # Return to project directory
 else
   echo "...Using Chrome from cache"
 fi
 
+# Download ChromeDriver matching the installed Chrome version
+CHROME_VERSION=$($STORAGE_DIR/chrome/opt/google/chrome/chrome --version | awk '{print $3}' | cut -d'.' -f1)
+echo "...Detected Chrome version: $CHROME_VERSION"
+
+if [[ ! -f $STORAGE_DIR/chromedriver ]]; then
+  echo "...Downloading ChromeDriver"
+  wget -P ./ "https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0/chromedriver_linux64.zip"
+  unzip chromedriver_linux64.zip -d $STORAGE_DIR/chrome
+  rm chromedriver_linux64.zip
+  mv $STORAGE_DIR/chrome/chromedriver $STORAGE_DIR/
+fi
+
+# Install Python dependencies
 pip install -r requirements.txt
